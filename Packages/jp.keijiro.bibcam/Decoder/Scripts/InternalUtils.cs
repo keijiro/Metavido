f70a48e4f96bc379916d557616192c6197ad62c0
Keijiro Takahashi <keijiro@gmail.com>
2021-11-21 22:29:44 +0900

    Tweaks

--- Packages/jp.keijiro.bibcam/Decoder/Scripts/InternalUtils.cs
+++ Packages/jp.keijiro.bibcam/Decoder/Scripts/InternalUtils.cs
@@ -6,10 +6,12 @@ namespace Bibcam.Decoder {
 static class GfxUtil
 {
     public static RenderTexture RGBARenderTexture(int width, int height)
-      => new RenderTexture(width, height, 0);
+      => new RenderTexture(width, height, 0)
+           { wrapMode = TextureWrapMode.Clamp };
 
     public static RenderTexture RHalfRenderTexture(int width, int height)
-      => new RenderTexture(width, height, 0, RenderTextureFormat.RHalf);
+      => new RenderTexture(width, height, 0, RenderTextureFormat.RHalf)
+           { wrapMode = TextureWrapMode.Clamp };
 
     public static GraphicsBuffer StructuredBuffer(int count, int stride)
       => new GraphicsBuffer(GraphicsBuffer.Target.Structured, count, stride);
