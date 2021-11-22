8a40d7ae4767525db128050d7a6d109fe1ad390d
Keijiro Takahashi <keijiro@gmail.com>
2021-11-22 16:45:29 +0900

    Aspect ratio conversion

--- Packages/jp.keijiro.bibcam/Decoder/Scripts/BibcamCameraController.cs
+++ Packages/jp.keijiro.bibcam/Decoder/Scripts/BibcamCameraController.cs
@@ -17,9 +17,22 @@ public sealed class BibcamCameraController : MonoBehaviour
     {
         var meta = _decoder.Metadata;
         if (!meta.IsValid) return;
+
+        var camera = GetComponent<Camera>();
+
+        // Projection matrix with aspect ratio conversion
+        var proj = meta.ProjectionMatrix;
+        if (camera.aspect < 16.0f / 9)
+            proj[1, 1] = proj[0, 0] * camera.aspect;
+        else
+            proj[0, 0] = proj[1, 1] / camera.aspect;
+
+        // Transform update
         transform.position = meta.CameraPosition;
         transform.rotation = meta.CameraRotation;
-        GetComponent<Camera>().projectionMatrix = meta.ProjectionMatrix;
+
+        // Camera parameter update
+        camera.projectionMatrix = proj;
     }
 
     #endregion
