06f0b072565d1eb0757a91c45819d8e89372c4e8
Keijiro Takahashi <keijiro@gmail.com>
2021-12-02 14:08:01 +0900

    Add HAP player support

--- Packages/jp.keijiro.bibcam/Decoder/Scripts/BibcamVideoFeeder.cs
+++ Packages/jp.keijiro.bibcam/Decoder/Scripts/BibcamVideoFeeder.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+
 #if BIBCAM_HAS_UNITY_VIDEO
 using UnityEngine.Video;
 #endif
+
+#if BIBCAM_HAS_KLAK_HAP
+using Klak.Hap;
+#endif
+
+#pragma warning disable CS0414
 
 namespace Bibcam.Decoder {
 
 sealed class BibcamVideoFeeder : MonoBehaviour
 {
-#if BIBCAM_HAS_UNITY_VIDEO
-
     #region Scene object reference
 
     [SerializeField] BibcamMetadataDecoder _decoder = null;
@@ -18,22 +23,37 @@ sealed class BibcamVideoFeeder : MonoBehaviour
 
     #region MonoBehaviour implementation
 
+    RenderTexture _rt;
+
+    void Start()
+    {
+        _rt = RenderTexture.GetTemporary(1920, 1080);
+
+#if BIBCAM_HAS_UNITY_VIDEO
+        var video = GetComponent<VideoPlayer>();
+        if (video != null)
+        {
+            video.renderMode = VideoRenderMode.RenderTexture;
+            video.targetTexture = _rt;
+        }
+#endif
+
+#if BIBCAM_HAS_KLAK_HAP
+        var hap = GetComponent<HapPlayer>();
+        if (hap != null) hap.targetTexture = _rt;
+#endif
+    }
+
+    void OnDestroy()
+      => RenderTexture.ReleaseTemporary(_rt);
+
     void Update()
     {
-        var video = GetComponent<VideoPlayer>();
-        if (video.texture == null) return;
-        _decoder.Decode(video.texture);
-        _demuxer.Demux(video.texture, _decoder.Metadata);
+        _decoder.Decode(_rt);
+        _demuxer.Demux(_rt, _decoder.Metadata);
     }
 
     #endregion
-
-#else
-
-    void OnValidate()
-      => Debug.LogError("UnityEngine.Video is missing.");
-
-#endif
 }
 
 } // namespace Bibcam.Decoder
