3ca8fa2cedd7438e3dc8eab2fcae75276a70469b
Keijiro Takahashi <keijiro@gmail.com>
2024-08-26 16:35:16 +0900

    Decoder argument support

---
+++ Assets/Decoder/ArgsParser.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace Metavido.Decoder {
+
+public sealed class ArgsParser : MonoBehaviour
+{
+    void Start()
+    {
+        var args = System.Environment.GetCommandLineArgs();
+        for (var i = 1; i < args.Length; i++)
+            if (i < args.Length - 1 && args[i] == "--sourceURL")
+                GetComponent<VideoPlayer>().url = args[++i];
+    }
+}
+
+} // namespace Metavido.Decoder
